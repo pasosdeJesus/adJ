1a6f6a9159ea326701fd681f4b927e28d6d466cd
vtamara <>
2004-05-03 09:15:24 +0000

    Publicado en proyecto Gutenberg. Hechos hasta 22,5

--- biblia_dp/libros.sh
+++ biblia_dp/libros.sh
@@ -5,8 +5,8 @@ function genlib {
 	./libro.sh .. $1 "$2: Traducción de dominio público abierta a mejoras" "$3" "$4" "$5" "$6"
 }
 
-genlib mateo_dp "Buena Nueva de acuerdo a Mateo" "mateo.gbfxml" "1ev" 11500 "29 February 2004"
-genlib marcos_dp "Buena Nueva de acuerdo a Marcos" "marcos.gbfxml" "1ev" 11501 "29 February 2004"
-genlib lucas_dp "Buena Nueva de acuerdo a Lucas" "lucas.gbfxml" "1ev" 11502 "29 February 2004"
-genlib juan_dp "Buena Nueva de acuerdo a Juan" "juan.gbfxml" "1ev" 11503 "29 February 2004"
+genlib mateo_dp "Buena Nueva de acuerdo a Mateo" "mateo.gbfxml" "1ev" 12500 "2 May 2004"
+genlib marcos_dp "Buena Nueva de acuerdo a Marcos" "marcos.gbfxml" "1ev" 12501 "2 May 2004"
+genlib lucas_dp "Buena Nueva de acuerdo a Lucas" "lucas.gbfxml" "1ev" 12502 "2 May 2004"
+genlib juan_dp "Buena Nueva de acuerdo a Juan" "juan.gbfxml" "1ev" 12503 "2 May 2004"
 genlib evangelios_dp "Los Evangelios" "intro-evangelios.gbfxml mateo.gbfxml marcos.gbfxml lucas.gbfxml juan.gbfxml" "4ev" 0 "No"
