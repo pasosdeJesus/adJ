77901e1c1e75d5de04695e91d535a6d33e9d6943
vtamara <>
2006-02-03 10:00:27 +0000

    5,28

--- biblia_dp/fixldevdbrep.sh
+++ biblia_dp/fixldevdbrep.sh
@@ -1,20 +1,54 @@
 #!/bin/sh
-# Ensure that some files are links to development files of docbook/repasa
-# Public domain. 2003
+# Arregla enlaces en sitio de desarrollo.
+# Public domain. 2006
+
+op=$1;
+
+cmd="ln -s"
+if (test "$op" = "-cvs") then {
+	cmd="cp"
+} fi;
 
 if (test "$DREPASA" = "") then {
 	DREPASA=$HOME/structio/repasa;
 } fi;
 
+function opera {
+	f=$1;
+	d=$2;
+	diff $f $d > /dev/null
+	if (test "$?" != "0") then {
+		echo "No enlace por diferencias entre $d $f";
+	} 
+	else {
+		rm -f $d;
+		eval "$cmd $f $d"
+	} fi;
+}
 
-rm -f conf.sh docbookrep_html.xsl docbookrep_tex.dsl docbookrep_html.dsl
-ln -s $DREPASA/docbook/conf.sh $DREPASA/docbook/docbookrep_html.xsl $DREPASA/docbook/docbookrep_tex.dsl $DREPASA/docbook/docbookrep_html.dsl .
+opera $DREPASA/docbook/conf.sh conf.sh
+opera $DREPASA/docbook/docbookrep_html.xsl docbookrep_html.xsl
+opera $DREPASA/docbook/docbookrep_tex.dsl docbookrep_tex.dsl
+opera $DREPASA/docbook/docbookrep_html.dsl docbookrep_html.dsl
 
-cd herram
-rm -f comdocbook.mak conthtmldoc.awk comdist.mak confaux.sh db2rep misc.sh
-ln -s $DREPASA/docbook/herram/comdocbook.mak $DREPASA/docbook/herram/conthtmldoc.awk $DREPASA/docbook/herram/comdist.mak $DREPASA/docbook/herram/confaux.sh $DREPASA/docbook/herram/db2rep $DREPASA/docbook/herram/misc.sh .
+opera $DREPASA/docbook/herram/comdocbook.mak herram/comdocbook.mak
+opera $DREPASA/docbook/herram/conthtmldoc.awk herram/conthtmldoc.awk
+opera $DREPASA/docbook/herram/comdist.mak herram/comdist.mak
+opera $DREPASA/docbook/herram/confaux.sh herram/confaux.sh
+opera $DREPASA/docbook/herram/db2rep herram/db2rep
+opera $DREPASA/docbook/herram/misc.sh herram/misc.sh
 
-cd ../img
-rm -f home.png prev.png toc-minus.png blank.png important.png toc-plus.png caution.png next.png tip.png up.png draft.png note.png toc-blank.png warning.png
-ln -s $DREPASA/docbook/img/home.png $DREPASA/docbook/img/prev.png $DREPASA/docbook/img/toc-minus.png $DREPASA/docbook/img/blank.png $DREPASA/docbook/img/important.png $DREPASA/docbook/img/toc-plus.png $DREPASA/docbook/img/caution.png $DREPASA/docbook/img/next.png $DREPASA/docbook/img/tip.png $DREPASA/docbook/img/up.png $DREPASA/docbook/img/draft.png $DREPASA/docbook/img/note.png $DREPASA/docbook/img/toc-blank.png $DREPASA/docbook/img/warning.png .
- 
+opera $DREPASA/docbook/img/home.png img/home.png
+opera $DREPASA/docbook/img/prev.png img/prev.png
+opera $DREPASA/docbook/img/toc-minus.png img/toc-minus.png
+opera $DREPASA/docbook/img/blank.png img/blank.png
+opera $DREPASA/docbook/img/important.png img/important.png
+opera $DREPASA/docbook/img/toc-plus.png img/toc-plus.png
+opera $DREPASA/docbook/img/caution.png img/caution.png
+opera $DREPASA/docbook/img/next.png img/next.png
+opera $DREPASA/docbook/img/tip.png img/tip.png
+opera $DREPASA/docbook/img/up.png img/up.png
+opera $DREPASA/docbook/img/draft.png img/draft.png
+opera $DREPASA/docbook/img/note.png img/note.png
+opera $DREPASA/docbook/img/toc-blank.png img/toc-blank.png
+opera $DREPASA/docbook/img/warning.png img/warning.png
