7cac475e24219a501d0e0b8d8400c513a8d95962
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2013-02-23 18:44:54 -0500

    Variables para prepsendmail mejores

--- arboldd/usr/local/share/examples/adJ/varcorreo.sh
+++ arboldd/usr/local/share/examples/adJ/varcorreo.sh
@@ -1,77 +1,39 @@
 #!/bin/sh
-# Variables para servidor con adJ
+# Variables para servidor de correo seguro sobre adJ
 
-
-#### MONITOREO
-ipmaqint=192.168.191.1
-ipotraorg=180.126.248.410
-nomotraorg=www.otra.org
-nomotraorg2=
-rutamon=/var/adJ/monitorea/
-
-
-#### BASES DE DATOS SEGURAS
-
-#### SERVIDOR DE CORREO SEGURO
 # Particion de correo y posible imagen Encriptada
 partenc=/var/maildir
-imgenc=/var/datos.img
-svnimg=svnd1
-tamimg=120000000
+imgenc=
+svnimg=
+tamimg=
 
 # Particion de respaldo e imagen encriptada de respaldo
-partenc2=/var/respaldo
-imgenc2=/var/respdatos.img
-svnimg2=svnd2
-tamimg2=120000000
+partenc2=
+imgenc2=
+svnimg2=
+tamimg2=
 
 # Directorio con correos (prefiera Maildir)
 Maildir=Maildir
 
-# Grupo en servidor
-grupous=grupo
+# Grupo en servidor de los usuarios de correo (en blanco si no hay uno)
+grupous=
 
 ### RESPALDO EN OTRO SERVIDOR DE CORREO SEGURO AMIGO
 # Servidor de respaldo 
-sresp="192.168.181.5"
+sresp="otroservidor.pasosdeJesus.org"
 
 # Opciones para conectarse a servidor de respaldo
 opsresp=""
 
 # Usuario en servidor de respaldo que hace mantenimientos
-usrespaldo=org
+usrespaldo=usuarioremoto
 
 # Usuario en computador local que sincroniza con servidor de respaldo
-usrespaldolocal=usuario
+usrespaldolocal=usuariolocal
 
 # Nombre de la organización ante servidor de respaldo --usado para grupo y posfijo de cuentas
-orgrespaldo=org
+orgrespaldo=miorg
 
 # Dominio con el que se configuraran cuentas virtuales en servidor de respaldo
-domrespaldo=reiniciar.org
-
-
-#### SERVIDOR WEB CON CMS DRUPAL
-#
-# Respaldos drupal, por defecto base de datos diaria del ultimo mes y
-# directorio de sitios de martes, jueves y domingo.
-
-# Directorio donde quedaran los respaldos 
-DIRRESPDRUPAL=/var/respaldo/drupal
-
-# Usuario de base de datos, el usuario del sistema que ejecuta debe tener un archivo .pgpass con la clave para este usuario, asi como para el usuario postgres
-DBUSUARIODRUPAL=drupal
-
-# Nombre de base drupal
-BASEDRUPAL=drupal3
-
-# Opcional. Nombre de una segunda base drupal
-BASEDRUPAL2=
-
-# Directorio con sitios 
-SITESDRUPAL=/var/www/htdocs/drupal6/sites
-
-# Opcional. Segundo directorio de sitios
-SITESDRUPAL2=
-
-
+domrespaldo=miorg.org
