c70a373ec0f1aa055c9d05c509fb238318522aaf
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2016-06-20 20:50:01 -0500

    actualizacions a 5.9

--- arboldd/usr/local/adJ/inst-adJ.sh
+++ arboldd/usr/local/adJ/inst-adJ.sh
@@ -1037,6 +1037,42 @@ if (test -f /usr/bin/sudo) then {
   rm -f /usr/libexec/sudo_noexec.so
 } fi;
 
+if (test -f /usr/share/misc/termcap.db) then {
+	vac="$vac 5.8 a 5.9";	
+	echo "Aplicando actualizaciones de 5.8 a 5.9 " >> /var/www/tmp/inst-adJ.bitacora;
+	rm -f /usr/libexec/smtpd/makemap
+
+	# opensmtpd-extras incluye los siguientes
+	rm -f /usr/libexec/smtpd/table-ldap
+	rm -f /usr/libexec/smtpd/table-passwd
+	rm -f /usr/libexec/smtpd/table-sqlite
+	rm -f /usr/share/man/man5/table_passwd.5
+
+	rm -f /usr/share/misc/termcap.db /usr/share/misc/terminfo.db
+
+	rm -f /usr/X11R6/include/intel_*.h
+	rm -f /usr/X11R6/include/r600_pci_ids.h
+	rm -f /usr/X11R6/include/radeon_*.h
+
+	rm -f /usr/include/malloc.h
+
+	rm -f /usr/libexec/auth/login_tis
+	rm -f /etc/rc.d/yppasswdd /usr/sbin/rpc.yppasswdd
+
+	cd /usr/X11R6/include/freetype2
+	rm -rf config
+	rm -f freetype.h ftadvanc.h ftbbox.h ftbdf.h ftbitmap.h ftbzip2.h \
+          ftcache.h ftcffdrv.h ftchapters.h ftcid.h fterrdef.h \
+          fterrors.h ftfntfmt.h ftgasp.h ftglyph.h ftgxval.h ftgzip.h \
+          ftimage.h ftincrem.h ftlcdfil.h ftlist.h ftlzw.h ftmac.h \
+          ftmm.h ftmodapi.h ftmoderr.h ftotval.h ftoutln.h ftpfr.h \
+          ftrender.h ftsizes.h ftsnames.h ftstroke.h ftsynth.h \
+          ftsystem.h fttrigon.h fttypes.h ftwinfnt.h t1tables.h \
+          ttnameid.h tttables.h tttags.h ttunpat.h
+
+} fi;
+	
+
 if  (test "$vac" != "") then {
 	dialog --title 'Actualizaciones aplicadas' --msgbox "\\nSe aplicaron actualizaciones: $vac\\n\\n$mac\\n" 15 60
 } fi;
