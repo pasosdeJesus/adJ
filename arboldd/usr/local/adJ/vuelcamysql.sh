8141900b10e2df93f984b46b07bad6f71f22ab51
Vladimir Tamara <vtamara@pasosdeJesus.org>
2016-07-12 21:57:43 -0500

    Vuelca base mysql

---
+++ arboldd/usr/local/adJ/vuelcamysql.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Saca copia  de una base de datos MySQL en SQL y XML
+# Dominio publico. 2016. vtamara@pasosdeJesus.org
+
+bd="$1"
+usu="$2"
+cla="$3"
+
+if (test "$bd" = "") then {
+	echo "Falta primer parametro con nombre de base mysql"
+	exit 1;
+} fi;
+if (test "$usu" = "") then {
+	echo "Falta segundo parametro con usuario mysql"
+	exit 1;
+} fi;
+if (test "$cla" = "") then {
+	echo "Falta tercer parametro con clave mysql"
+	exit 1;
+} fi;
+dm=`date "+%d"` # Dia del mes
+/usr/local/bin/mysqldump --xml -c -u $usu --password=$cla $bd > /var/www/resbase/$bd-$dm.xml
+/usr/local/bin/mysqldump --compatible=postgresql -c -u $usu --password=$cla $bd > /var/www/resbase/$bd-$dm.sql
