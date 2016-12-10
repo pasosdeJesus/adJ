40fc656fa90ef1c864421a7b1991b9299dcdb3de
Vladimir Tamara <vtamara@pasosdeJesus.org>
2016-12-10 06:32:53 -0500

    Archivos de ordenes para volcar bases mysql y postgresql completas

---
+++ arboldd/usr/local/adJ/vuelcatodomysql.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Saca copia de todas las bases MySQL en SQL y XML
+# Dominio público. vtamara@pasosdeJesus.org. 2016
+
+
+usu="$1"
+cla="$2"
+
+if (test "$usu" = "") then {
+	echo "Falta primer parametro con usuario root mysql"
+	exit 1;
+} fi;
+if (test "$cla" = "") then {
+	echo "Falta segundo parametro con clave mysql"
+	exit 1;
+} fi;
+dm=`date "+%d"` # Dia del mes
+/usr/local/bin/mysqldump --force --xml -c -u $usu --password=$cla --all-databases > /var/www/resbase/mysql/mysql-$dm.xml
+/usr/local/bin/mysqldump --compatible=postgresql -c -u $usu --password=$cla --all-databases > /var/www/resbase/mysql/mysql-$dm.sql
+
---
+++ arboldd/usr/local/adJ/vuelcatodopg.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+# Saca copia de todas las bases PostgreSQL
+# Dominio público. vtamara@pasosdeJesus.org. 2016
+
+if (test "$SOCKPSQL" = "") then {
+	sockpsql="/var/www/var/run/postgresql"
+} else {
+	sockpsql="$SOCKPSQL"
+} fi;
+if (test "$ACUSPOS" = "") then {
+	acuspos="-Upostgres"
+} else {
+	acuspos="$ACUSPOS"
+} fi;
+if (test "$USPOS" = "") then {
+	uspos="postgres"
+} else {
+	uspos="$USPOS"
+} fi;
+if (test "$RES" = "") then {
+	dm=`date "+%d"` # Dia del mes
+	res="/var/www/resbase/pg/pg-$dm.sql"
+} else {
+	res="$RES"
+} fi;
+
+mkdir -p `dirname $res`
+touch $res
+chown _postgresql:_postgresql $res
+touch $res-t
+chmod +x `dirname $res`
+chown _postgresql:_postgresql $res-t
+rm -f /tmp/penc.txt
+echo "psql -h$sockpsql $acuspos -c 'SHOW SERVER_ENCODING' > /tmp/penc.txt" > /tmp/cu.sh
+chmod +x /tmp/cu.sh
+su - _postgresql /tmp/cu.sh 
+if (test -f /tmp/penc.txt -a ! -z /tmp/penc.txt) then {
+	dbenc=`grep -v "(1 row)" /tmp/penc.txt | grep -v "server_encoding" | grep -v "[-]-----" | grep -v "^ *$" | sed -e "s/  *//g"`
+} fi;
+echo "pg_dumpall $acuspos --inserts --column-inserts --host=$sockpsql > $res-t" > /tmp/cu.sh
+echo "if (test \"\$?\" != \"0\") then {" >> /tmp/cu.sh 
+echo "  echo \"No pudo completarse la copia\";" >> /tmp/cu.sh 
+echo "  exit 1;" >> /tmp/cu.sh 
+echo "} fi;" >> /tmp/cu.sh 
+chmod +x /tmp/cu.sh
+cat /tmp/cu.sh 
+su - _postgresql /tmp/cu.sh 
+if (test "$?" != "0" -a -f $res-t) then {
+	echo "* Copia incompleta dejada en $res-par"
+	mv $res-t $res-par
+} else {
+	grep "CREATE DATABASE" $res-t | grep -v "ENCODING" > /tmp/cb.sed
+	sed -e "s/\(.*\);$/s\/\1;\/\1 ENCODING='$dbenc';\/g/g" /tmp/cb.sed  > /tmp/cb2.sed
+	grep -v "ALTER ROLE $uspos" $res-t | sed -f /tmp/cb2.sed > $res
+} fi;
+
