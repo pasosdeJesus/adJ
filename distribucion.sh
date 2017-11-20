84ded9e58ff0ee97121a6d6bda17560b86851660
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2017-11-20 13:47:19 -0500

    no salta temp

--- distribucion.sh
+++ distribucion.sh
@@ -169,7 +169,6 @@ if (test "$sn" = "s") then {
 	} fi;
 
 	cd /sys
-	function saltatemp {
 	$dini/hdes/servicio-kernel.sh 2>&1 |  tee -a /var/www/tmp/distrib-adJ.bitacora	
 	# Esta en general se cambiaron comentarios a lo largo de todas
     # las fuentes.  Ver documentación en 
@@ -177,7 +176,6 @@ if (test "$sn" = "s") then {
 
 	# Para compilar vmstat
 	cp /sys/uvm/uvm_extern.h /usr/include/uvm/ 2>&1 |  tee -a /var/www/tmp/distrib-adJ.bitacora
-}
 
 	cd /sys/arch/$ARQ/conf 2>&1 >> /var/www/tmp/distrib-adJ.bitacora
 	echo "pwd=" `pwd` 2>&1 |  tee -a /var/www/tmp/distrib-adJ.bitacora
