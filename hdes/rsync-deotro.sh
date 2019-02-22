e358b43a77cc4723512542474a86cc61abdd14a8
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-02-21 21:03:24 -0500

    rsync que borra

--- hdes/rsync-deotro.sh
+++ hdes/rsync-deotro.sh
@@ -2,7 +2,7 @@
 
 . ./ver.sh
 
-cmd="rsync --delete -e \"ssh $OPSSHOTRO\" -ravcz $USOTRO@$OTRO:$DIROTRO/adJ/$V$VESP-$ARQ/* $V$VESP-$ARQ/"
+cmd="rsync --delete -e \"ssh $OPSSHOTRO\" -ravcz $USOTRO@$OTRO:$DIROTRO/adJ/$V$VESP-$ARQ/ $V$VESP-$ARQ/"
 echo $cmd
 eval $cmd
 
