219a63d9cf13a5f4ecae089b60ce90ec99e5bdf3
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-02-20 10:34:52 -0500

    ajusta a 6.6

--- distribucion.sh
+++ distribucion.sh
@@ -790,6 +790,8 @@ if (test "$sn" = "s") then {
 	# Modificados para posibilitar compilación
 	# Deben estar en mystuff
 
+	paquete gettext-tools
+	exit 1
 	# Todo lo de perl tuvo que recompilarse
 	# evita error loadable library and perl binaries are mismatched (got handshake key 0xca80000, needed 0xcd80000)
 	# Si por ejemplo es:
