789e2e355c565e53f20afadcb52fd4e95aa295df
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-05-27 03:15:20 -0500

    versiculos

--- hdes/rsync-aotro.sh
+++ hdes/rsync-aotro.sh
@@ -2,11 +2,11 @@
 
 . ./ver.sh
 
-cmd="rsync -vrt --size-only --delete -e \"ssh $OPSSHOTRO\" $V$VESP-$ARQ/ $USOTRO@$OTRO:$DIROTRO/adJ/$V$VESP-$ARQ"
+cmd="rsync -vrt --size-only --delete -e \"ssh $OPSSHOTRO\" $V$VESP-$ARQ/ $USOTRO@$OTRO:$DIROTRO/$V$VESP-$ARQ"
 echo $cmd
 eval $cmd
 
-cmd="rsync --delete -e \"ssh $OPSSHOTRO\" $V$VESP-$ARQ/ -ravcz $USOTRO@$OTRO:$DIROTRO/adJ/$V$VESP-$ARQ"
+cmd="rsync --delete -e \"ssh $OPSSHOTRO\" $V$VESP-$ARQ/ -ravcz $USOTRO@$OTRO:$DIROTRO/$V$VESP-$ARQ"
 echo $cmd
 eval $cmd
 
