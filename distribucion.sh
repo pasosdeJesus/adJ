0d373838d2b7228e7b90d19be66f585cc2c26683
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-01-09 18:13:58 -0500

    Logra compilar firefox

--- distribucion.sh
+++ distribucion.sh
@@ -803,12 +803,6 @@ if (test "$sn" = "s") then {
   # Modificados para posibilitar compilación
   # Deben estar en mystuff
 
-  paquete colorls
-  paquete firefox-esr
-
-
-
-  exit 1
   # Todo lo de perl tuvo que recompilarse
   # evita error loadable library and perl binaries are mismatched (got handshake key 0xca80000, needed 0xcd80000)
   # Si por ejemplo es:
