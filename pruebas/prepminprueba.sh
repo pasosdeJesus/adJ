124a8441f75547cd8241f8dbec77a869355e63ca
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2026-04-05 15:51:07 -0400

    parche 5 actualizado, pruebas pasan

--- pruebas/prepminprueba.sh
+++ pruebas/prepminprueba.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
 # Copia lo minimo necesario para aplicar los parches y recompilar libc
+# Dominio público. 2026. vtamara@pasosdeJesus.org
 
 . ./ver.sh
 
-rsync -ravz --delete /usr/src$VP-orig/regress/ /usr/src/regress/
-rsync -ravz --delete /usr/src$VP-orig/lib/libc/ /usr/src/lib/libc/
-rsync -ravz --delete /usr/src$VP-orig/share/locale/ /usr/src/share/locale/
-rsync -ravz --delete /usr/src$VP-orig/usr.bin/colldef /usr/src/usr.bin/colldef
-rsync -ravz --delete /usr/src$VP-orig/usr.bin/localedef /usr/src/usr.bin/localedef
-rsync -ravz --delete /usr/src$VP-orig/gnu/usr.bin/perl/hints/ /usr/src/gnu/usr.bin/perl/hints/
-for i in /gnu/llvm/clang/lib/Basic/Targets/OSTargets.h /gnu/llvm/libcxx/include/__config /gnu/llvm/libcxx/include/__locale /gnu/llvm/libcxx/include/__support/openbsd/xlocale.h /gnu/usr.bin/perl/hints/openbsd.sh /include/Makefile /include/ctype.h /include/langinfo.h /include/locale.h /include/monetary.h /include/stdio.h /include/string.h /include/strings.h /include/time.h /include/wchar.h /include/wctype.h /include/xlocale.h /share/locale/cldr/Makefile /usr.sbin/pkg_add/OpenBSD/BaseState.pm /usr.sbin/pkg_add/OpenBSD/PackingElement.pm /gnu/lib/libcxx/include/c++/v1/__config_site /gnu/llvm/libcxx/include/__config_site.in /gnu/llvm/libcxx/include/__locale_dir/locale_base_api.h /gnu/gcc/gcc/config/openbsd.h /etc/Makefile /etc/etc.amd64/Makefile.inc /include/Makefile /usr.bin/Makefile ; do
-	rsync -ravz --delete /usr/src$VP-orig/$i /usr/src/$i
+# 1. Limpieza de archivos de parches en la raíz para que aplicahasta opere de cero
+doas rm -f /usr/src/*.patch
+
+# 2. Restauración completa de directorios base modificados
+# Sincronizamos todo regress/lib/libc como sugeriste para evitar parches "reversos"
+for d in regress/lib/libc lib/libc share/locale gnu/llvm/clang gnu/llvm/libcxx gnu/lib/libcxx gnu/usr.bin/perl ; do
+	if [ -d /usr/src$VP-orig/$d ]; then
+		doas rsync -ravz --delete /usr/src$VP-orig/$d/ /usr/src/$d/
+	fi
 done
 
-rm -rf /usr/src/*patch
+# 3. Restauración de archivos específicos fuera de los directorios anteriores
+for i in sys/sys/localedef.h include/Makefile include/ctype.h include/langinfo.h include/locale.h include/monetary.h include/stdio.h include/string.h include/strings.h include/time.h include/wchar.h include/wctype.h include/xlocale.h share/locale/cldr/Makefile usr.sbin/pkg_add/OpenBSD/BaseState.pm usr.sbin/pkg_add/OpenBSD/PackingElement.pm gnu/gcc/gcc/config/openbsd.h etc/Makefile etc/etc.amd64/Makefile.inc usr.bin/Makefile ; do
+	if [ -e /usr/src$VP-orig/$i ]; then 
+		doas cp /usr/src$VP-orig/$i /usr/src/$i
+	else 
+		doas rm -f /usr/src/$i
+	fi
+done
 
+# 4. Eliminar directorios que adJ crea y que no existen en OpenBSD
+doas rm -rf /usr/src/usr.bin/localedef /usr/src/usr.bin/colldef
